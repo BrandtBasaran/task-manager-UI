1b483639b2a7f9baf5e741cf7320dce561b1360a
brandt <brandtbasaran@gmail.com>
2022-12-17 16:31:26 -0500

    added dummy response to GET

--- test.js
+++ test.js
@@ -2,23 +2,31 @@ const taskId = `id`;
 const taskList = `tasklist`;
 const taskName = `taskname`;
 
-const httpGet = function(url) {
+const httpGet = function (url) {
     const options = {
         method: "GET",
         mode: 'no-cors'
     }
 
     fetch(url, options)
-    .then( response => {
-        console.log(JSON.stringify(response));
-        document.getElementById('tasklist').innerHTML = response.body;
-    })
-    .catch(error => {
-        console.error(error);
-    });
+        .then(response => {
+            response = {
+                "message": "Task GET succesful",
+                "status": "success",
+                "task": {
+                    "taskId": 12345,
+                    "taskName": "take out trash",
+                    "due-date": "00/22/11"
+                }
+            }
+            console.log(JSON.stringify(response));
+        })
+        .catch(error => {
+            console.error(error);
+        });
 };
 
-const getData = function() {
+const getData = function () {
     const id = document.getElementById(taskId).value;
     const response = httpGet(`https://q36ezw76zh.execute-api.us-east-1.amazonaws.com/task/${id}`);
     console.log(response);
@@ -37,26 +45,27 @@ function httpPut(url) {
     }
 
     fetch(url, options)
-    .then(response => {
+        .then(response => {
             console.log(JSON.stringify(response));
-            document.getElementById('tasklist').innerHTML = response.body;})
-    .catch(error => {
-        console.log('brandt messed up the puts');
-        console.error(error);
-    });
+            document.getElementById('tasklist').innerHTML = response.body;
+        })
+        .catch(error => {
+            console.log('brandt messed up the puts');
+            console.error(error);
+        });
 }
 
-var putData = function() {
+var putData = function () {
     var id = document.getElementById('id').textContent;
     httpPut(`https://q36ezw76zh.execute-api.us-east-1.amazonaws.com/task/${id}`);
-    console.log(response);  
+    console.log(response);
 }
 
-var postData = function() {
+var postData = function () {
     var id = document.getElementById('id').textContent;
     let url = `https://q36ezw76zh.execute-api.us-east-1.amazonaws.com/task/${id}`;
-    let data = {'TaskId': `${document.getElementById('id')}`, 'TaskName': `${document.getElementById('taskname')}`, 'Task-List': `${document.getElementById('tasklist')}`};
-    
+    let data = { 'TaskId': `${document.getElementById('id')}`, 'TaskName': `${document.getElementById('taskname')}`, 'Task-List': `${document.getElementById('tasklist')}` };
+
     fetch(url, {
         method: 'POST',
         headers: {
@@ -66,16 +75,17 @@ var postData = function() {
     }).then(response => {
         if (response.ok) {
 
-        console.log(JSON.stringify(response));
-        document.getElementById('tasklist').innerHTML = response.body;
+            console.log(JSON.stringify(response));
+            document.getElementById('tasklist').innerHTML = response.body;
 
-    } else {
-        return `HTTP error: ${response.status}`;
-    }})
-    .catch(error => {
-        console.log('brandt messed up the post');
-        console.error(error);
-    });;
+        } else {
+            return `HTTP error: ${response.status}`;
+        }
+    })
+        .catch(error => {
+            console.log('brandt messed up the post');
+            console.error(error);
+        });;
 }
 
 var deleteData = function () {
